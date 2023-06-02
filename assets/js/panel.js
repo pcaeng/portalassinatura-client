855b234e878fc8ab4570e39b2052761018a983fb
Arthur Rubi <arthur.rubi1594@gmail.com>
2023-06-02 09:19:13 -0300

    Início da tela de assinatura, modal de configuração concluído e melhorias de estilo.

--- assets/js/panel.js
+++ assets/js/panel.js
@@ -4,9 +4,6 @@ function goToFirstStep() {
 
     const secondStepElement = document.getElementById("panel-step-2");
     secondStepElement.hidden = true;
-
-    const lastStepElement = document.getElementById("panel-step-3");
-    lastStepElement.hidden = true;
 }
 
 function goToSecondStep() {
@@ -21,12 +18,30 @@ function goToSecondStep() {
 }
 
 function goToLastStep() {
-    const firstStepElement = document.getElementById("panel-step-1");
-    firstStepElement.hidden = true;
-
     const secondStepElement = document.getElementById("panel-step-2");
     secondStepElement.hidden = true;
 
     const lastStepElement = document.getElementById("panel-step-3");
     lastStepElement.hidden = false;
+
+    const subscription = document.getElementById("panel-subscription");
+    subscription.hidden = true;
+}
+
+function startSubscription() {
+    const lastStepElement = document.getElementById("panel-step-3");
+    lastStepElement.hidden = true;
+
+    const subscription = document.getElementById("panel-subscription");
+    subscription.hidden = false;
+}
+
+function showSettings() {
+    const settingsElement = document.getElementById("panel-settings");
+    settingsElement.hidden = false;
+}
+
+function hideSettings() {
+    const settingsElement = document.getElementById("panel-settings");
+    settingsElement.hidden = true;
 }
